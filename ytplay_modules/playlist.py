6e251935a1ed58366e660b10109651d1506d9d8c
zbynekdrlik <drlik.zbynek@gmail.com>
2025-06-26 15:52:07 +0200

    Phase 14: Create placeholder playlist.py module

---
+++ ytplay_modules/playlist.py
@@ -0,0 +1,11 @@
+"""
+Placeholder module - to be implemented.
+"""
+
+def start_playlist_sync_thread():
+    """Placeholder."""
+    pass
+
+def trigger_manual_sync():
+    """Placeholder."""
+    pass
