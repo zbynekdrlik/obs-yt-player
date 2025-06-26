793fc5f4ad48abf6b69da9a0c2d7d19cbf838145
zbynekdrlik <drlik.zbynek@gmail.com>
2025-06-26 23:08:30 +0200

    Version 4.0.5: Added proper module exports to __init__.py

--- ytplay_modules/__init__.py
+++ ytplay_modules/__init__.py
@@ -1,4 +1,41 @@
 """
 OBS YouTube Player - Shared modules for all script instances
-Version 4.0.0 - Phase 14 Modular Architecture
+Version 4.0.5 - Phase 14 Modular Architecture
 """
+
+# Make modules available at package level for easier imports
+from . import config
+from . import state
+from . import logger
+from . import main
+from . import ui
+from . import tools
+from . import cache
+from . import scene
+from . import utils
+from . import playlist
+from . import download
+from . import metadata
+from . import gemini_metadata
+from . import normalize
+from . import playback
+from . import reprocess
+
+__all__ = [
+    'config',
+    'state',
+    'logger',
+    'main',
+    'ui',
+    'tools',
+    'cache',
+    'scene',
+    'utils',
+    'playlist',
+    'download',
+    'metadata',
+    'gemini_metadata',
+    'normalize',
+    'playback',
+    'reprocess'
+]
