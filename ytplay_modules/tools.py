53b6d16838b07845f45d549456bbdc00fb36beba
zbynekdrlik <drlik.zbynek@gmail.com>
2025-06-26 15:51:58 +0200

    Phase 14: Create placeholder tools.py module

---
+++ ytplay_modules/tools.py
@@ -0,0 +1,11 @@
+"""
+Placeholder module - to be implemented.
+This file ensures imports don't fail during migration.
+"""
+
+def start_tools_thread():
+    """Placeholder for tools thread."""
+    from logger import log
+    log("Tools module not yet migrated")
+
+# Add other placeholder functions as needed
