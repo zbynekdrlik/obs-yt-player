09d1c670a4228d9eb4fdd3689f3ffd16165b5c4e
zbynekdrlik <drlik.zbynek@gmail.com>
2025-07-09 10:06:10 +0200

    Create ytfast_modules directory in yt-player-main

---
+++ yt-player-main/ytfast_modules/__init__.py
@@ -0,0 +1,34 @@
+"""
+YouTube Player Modules
+======================
+
+Core modules for the OBS YouTube Player script.
+"""
+
+# Version info
+__version__ = "3.4.1"
+__author__ = "Your Name"
+
+# Module imports
+__all__ = [
+    'cache',
+    'config', 
+    'download',
+    'gemini_metadata',
+    'logger',
+    'media_control',
+    'metadata',
+    'normalize',
+    'opacity_control',
+    'playback',
+    'playback_controller',
+    'playlist',
+    'reprocess',
+    'scene',
+    'state',
+    'state_handlers',
+    'title_manager',
+    'tools',
+    'utils',
+    'video_selector'
+]
