40a9f03b26afee2b3a4062f38c42aeffbae84a44
zbynekdrlik <drlik.zbynek@gmail.com>
2025-12-25 00:00:41 +0100

    chore: Update installer commit ID

--- install.ps1
+++ install.ps1
@@ -18,7 +18,7 @@ $ProgressPreference = "SilentlyContinue"  # Faster downloads
 
 # Configuration
 $InstallerVersion = "1.1.0"
-$InstallerCommit = "975d7ce"  # Update on each commit
+$InstallerCommit = "c7ee344"  # Update on each commit
 $RepoOwner = "zbynekdrlik"
 $RepoName = "obs-yt-player"
 $ScriptFolder = "yt-player-main"
