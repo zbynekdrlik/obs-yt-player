a703f155b2b35757e81617dbbb86551d64b866c7
zbynekdrlik <drlik.zbynek@gmail.com>
2025-12-25 10:17:53 +0100

    chore: Update installer commit ID

--- install.ps1
+++ install.ps1
@@ -18,7 +18,7 @@ $ProgressPreference = "SilentlyContinue"  # Faster downloads
 
 # Configuration
 $InstallerVersion = "1.1.0"
-$InstallerCommit = "c2ff927"  # Update on each commit
+$InstallerCommit = "9b4992b"  # Update on each commit
 $RepoOwner = "zbynekdrlik"
 $RepoName = "obs-yt-player"
 $RepoBranch = "feature/powershell-installer"  # Branch to download from (when no release)
